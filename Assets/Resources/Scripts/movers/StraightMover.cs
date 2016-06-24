dda55dabf1f539431fa3a51bd2cb69f7dfb5ecfd
SexyTsunami <ashyguy55@gmail.com>
2016-06-24 00:12:44 -0700

    Revert "Revert "Art from other astroid game""
    This reverts commit 299c0d31cb9f7e909446bc4a644ca94c34739a6b.

---
+++ Assets/Resources/Scripts/movers/StraightMover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StraightMover : MonoBehaviour {
+	
+	public float speed;
+	public int angle = 0;
+	
+	protected virtual void Start ()
+	{
+		float angleRadians = Mathf.Deg2Rad * (-angle + 90f);
+		Vector3 shotDirection = new Vector3 (Mathf.Cos (angleRadians), 0, Mathf.Sin (angleRadians));
+		GetComponent<Rigidbody>().velocity = shotDirection * speed;
+	}
+
+}
