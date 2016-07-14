64218a7fdcc5729a0f86ba45f42784bdb66b1b1f
aaront98 <plainaaron@yahoo.com>
2016-07-13 23:04:53 -0700

    Added screen title!
    what i said in the title

---
+++ Assets/Resources/Scripts/controllers/Main.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour {
+
+    public Text startGameButton;
+    public GameObject menuScreen;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void StartGame() {
+        menuScreen.SetActive(false);
+    }
+}
