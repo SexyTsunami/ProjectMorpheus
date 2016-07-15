9c25324a6f935b1894f568695bc28c80eaf050a6
aaront98 <plainaaron@yahoo.com>
2016-07-14 23:34:54 -0700

    Added WASD + ArrowKey Movements

---
+++ Assets/Resources/Scripts/controllers/Movement2.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement2 : MonoBehaviour
+{ 
+
+float speed = 4f;
+
+// Update is called once per frame
+void Update()
+{
+
+    if (Input.GetKey(KeyCode.LeftArrow))
+    {
+        transform.Translate(-Vector2.right * speed * Time.deltaTime);
+    }
+    else if (Input.GetKey(KeyCode.RightArrow))
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+    else if (Input.GetKey(KeyCode.UpArrow))
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+    else if (Input.GetKey(KeyCode.DownArrow))
+    {
+        transform.Translate(-Vector2.up * speed * Time.deltaTime);
+        }
+    }
+}
